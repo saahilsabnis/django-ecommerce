557677f4fece686f22e2b4168b14e2181257eda2
Saahil Sabnis <saahilsabnis@gmail.com>
2022-06-28 17:37:07 +0530

    Fixed missing created field in Product model

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
