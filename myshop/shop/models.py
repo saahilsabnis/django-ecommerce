a383e72684ef7ed7fdb31cbde541d282ea866c26
Saahil Sabnis <saahilsabnis@gmail.com>
2022-06-29 05:00:00 +0530

    Added product catalog (Home Page) Views and Templates

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,6 +12,9 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
+    
     def __str__(self):
         return self.name
     
@@ -29,5 +33,9 @@ class Product(models.Model):
         ordering = ('name', )
         index_together = (('id', 'slug'), )
 
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])    
+
     def __str__(self):
         return self.name
+        
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 # Create your views here.
 
 def product_list(request, category_slug=None):
     category = None
-    categories = Category.object.all()
+    categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
@@ -13,5 +13,4 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, shop/product/detail.html, {'product':product})
-    
+    return render(request, 'shop/product/detail.html', {'product':product})
