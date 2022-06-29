6d861f057acc6c624c0f5498368ea429c3260a9f
Saahil Sabnis <saahilsabnis@gmail.com>
2022-06-30 03:15:48 +0530

    Added 'Add' products and 'Update' products functionality to the cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -14,12 +14,12 @@ class Cart(object):
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product.id] = {'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
         
         if override_quantity:
-            self.cart[product.id]['quantity'] = quantity
+            self.cart[product_id]['quantity'] = quantity
         else:
-            self.cart[product.id]['quantity'] += quantity
+            self.cart[product_id]['quantity'] += quantity
         self.save()
 
     def remove(self, product):
